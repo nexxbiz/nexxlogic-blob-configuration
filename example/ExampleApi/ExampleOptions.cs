c56fa89b61eedfa4cbf5a3bb4815a44f0198c197
Luuk ten Böhmer <l.ten.bohmer@betabit.nl>
2022-12-21 16:00:36 +0100

    Add smaller example settings

--- example/ExampleApi/ExampleOptions.cs
+++ example/ExampleApi/ExampleOptions.cs
@@ -2,7 +2,7 @@
 
 public class ExampleOptions
 {
-    public string LargeSetting1 { get; set; } = string.Empty;
-    public string LargeSetting2 { get; set; } = string.Empty;
+    public string Setting1 { get; set; } = string.Empty;
+    public string Setting2 { get; set; } = string.Empty;
     public int FolderSetting { get; set; }
 }
